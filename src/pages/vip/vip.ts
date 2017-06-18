162b6e7d93031ab163783c570bd36cffa5759857
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-06-19 01:39:29 +0400

    minor tweaks

--- src/pages/vip/vip.ts
+++ src/pages/vip/vip.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PurchaseVipPage } from '../purchase-vip/purchase-vip';
+import { Facebook } from '@ionic-native/facebook';
+
 /**
  * Generated class for the VIP page.
  *
@@ -13,14 +15,22 @@ import { PurchaseVipPage } from '../purchase-vip/purchase-vip';
   templateUrl: 'vip.html',
 })
 export class VIPPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private facebook: Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VIP');
   }
-  watchAd(){}
-  invite(){}
+  watchAd(){
+    // https://ionicframework.com/docs/native/admob-free/
+  }
+  invite(){
+    // https://developers.facebook.com/docs/applinks/overview - app links overview
+    // https://developers.facebook.com/docs/applinks/hosting-api - to hot a link to send in options
+    // https://ionicframework.com/docs/native/facebook/ - the invite docs
+
+    //this.facebook.appInvite({applink, photoUrl});
+  }
   viewPurchasePage(){
     this.navCtrl.push(PurchaseVipPage);
   }
